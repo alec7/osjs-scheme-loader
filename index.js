23a95333a38c82d553daf3d14941fcb0f3e8c3c1
Anders Evenrud <andersevenrud@gmail.com>
2017-07-13 10:32:49 +0200

    Fixed escaping for Webpack

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = function(content) {
-  const found = content.match(/<gui\-fragment\s+?data\-fragment\-external=\\"(.*)\\"\s+?\/>/g);
-  if ( found ) {
-    found.forEach((f) => {
-      let imp = f.split(/<gui\-fragment\s+?data\-fragment\-external=\\"(.*)\\"\s+?\/>/)[0];
-      imp = path.resolve(this.context, imp);
-      if ( imp && fs.existsSync(imp) ) {
-        const w = fs.readFileSync(imp, 'utf-9');
-        content = content.replace(f, w);
-      }
-    });
-  }
-
+  const found = content.match(/<gui\-fragment\s+data\-fragment\-external=\\"([A-z0-9\.\-_]+)\\"\s+\/>/g);
+  found.forEach((f) => {
+    let current = f.split(/(<gui\-fragment\s+?data\-fragment\-external=\\"([A-z0-9\.\-_]+)\\"\s+?\/>)/);
+    let filename = path.resolve(this.context, current[2]);
+    if ( current[2] && fs.existsSync(filename) ) {
+      const w = fs.readFileSync(filename, 'utf-8');
+      content = content.replace(current[1], w.replace(/(\r|\n)+/g, '').replace(/[\\$'"]/g, '\\$&'));
+    }
+  });
   return content;
 };
