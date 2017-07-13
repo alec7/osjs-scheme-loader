705e728838d78eff401443b8c75a93bba315b693
Anders Evenrud <andersevenrud@gmail.com>
2017-07-13 16:14:26 +0200

    Added some error handling

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 
 module.exports = function(content) {
   const found = content.match(/<gui\-fragment\s+data\-fragment\-external=\\"([A-z0-9\.\-_]+)\\"\s+\/>/g);
-  found.forEach((f) => {
-    let current = f.split(/(<gui\-fragment\s+?data\-fragment\-external=\\"([A-z0-9\.\-_]+)\\"\s+?\/>)/);
-    let filename = path.resolve(this.context, current[2]);
-    if ( current[2] && fs.existsSync(filename) ) {
-      const w = fs.readFileSync(filename, 'utf-8');
-      content = content.replace(current[1], w.replace(/(\r|\n)+/g, '').replace(/[\\$'"]/g, '\\$&'));
-    }
-  });
+  if ( found ) {
+    found.forEach((f) => {
+      let current = f.split(/(<gui\-fragment\s+?data\-fragment\-external=\\"([A-z0-9\.\-_]+)\\"\s+?\/>)/);
+      let filename = path.resolve(this.context, current[2]);
+      if ( current[2] && fs.existsSync(filename) ) {
+        const w = fs.readFileSync(filename, 'utf-8');
+        content = content.replace(current[1], w.replace(/(\r|\n)+/g, '').replace(/[\\$'"]/g, '\\$&'));
+      }
+    });
+  }
   return content;
 };
