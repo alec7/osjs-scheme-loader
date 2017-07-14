ccf9fe67e4a7d2bb26dca8c0e1e4fb369ddd8a88
Anders Evenrud <andersevenrud@gmail.com>
2017-07-15 01:42:48 +0200

    Baked in html-loader and stock icon support

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 const path = require('path');
 const fs = require('fs');
+const tmp = require('tmp');
+
+function replaceStockIcons(html) {
+  const re = /(data\-icon="stock\:\/+([^"]+)")/g;
+  const matches = html.match(re) || [];
+
+  matches.forEach(function(iter) {
+    const spl = html.split(re);
+    const newIcon = spl[2].replace(/^stock\:\/+/, '');
+    html = html.replace(spl[1], 'data-stock-icon="' + newIcon + '"');
+  });
+
+  return html;
+}
 
 module.exports = function(content) {
-  const found = content.match(/<gui\-fragment\s+data\-fragment\-external=\\"([A-z0-9\.\-_]+)\\"\s+\/>/g);
-  if ( found ) {
-    found.forEach((f) => {
-      let current = f.split(/(<gui\-fragment\s+?data\-fragment\-external=\\"([A-z0-9\.\-_]+)\\"\s+?\/>)/);
-      let filename = path.resolve(this.context, current[2]);
-      if ( current[2] && fs.existsSync(filename) ) {
-        const w = fs.readFileSync(filename, 'utf-8');
-        content = content.replace(current[1], w.replace(/(\r|\n)+/g, '').replace(/[\\$'"]/g, '\\$&'));
-      }
-    });
-  }
-  return content;
+  const callback = this.async();
+  const loadModule = this.loadModule;
+  const context = this.context;
+
+  let html = fs.readFileSync(this.resourcePath, 'utf-8');
+  html = replaceStockIcons(html);
+
+  const attrs = 'attrs[]=img:src&attrs[]=:data-icon';
+  const re = /(<gui\-fragment\s+data\-fragment\-external=\"([A-z0-9\.\-_]+)\"\s+\/>)/g;
+  const found = html.match(re) || [];
+
+  found.forEach(function(f) {
+    const curr = f.split(re);
+    const filename = path.resolve(context, curr[2]);
+    const replace = curr[1];
+    html = html.replace(replace, fs.readFileSync(filename, 'utf-8'));
+  });
+
+  tmp.file(function(err, path, fd, cleanup) {
+    if ( err ) {
+      callback(err);
+    } else {
+      html = replaceStockIcons(html);
+      //console.log(html);
+      html = html.replace(/\r?\n|\r/g, '').replace(/\s+/g, ' ');
+
+      fs.write(fd, html);
+
+      loadModule('html-loader?' + attrs + '!' + path, function(err, data) {
+        cleanup();
+
+        if ( err ) {
+          callback(err);
+        } else {
+          callback(null, data);
+        }
+      });
+    }
+  });
 };
